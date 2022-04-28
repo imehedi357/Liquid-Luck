b4201090c7ac5fa3dc26da94982dd83fff21ba81
imehedi357 <imehedi357@gmail.com>
2022-04-28 20:46:28 +0600

    Initial commit

---
+++ SUST_Life/src/main/java/liquid_luck/sust_life/SUST_Life.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package liquid_luck.sust_life;
+
+/**
+ *
+ * @author imehe
+ */
+public class SUST_Life {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
