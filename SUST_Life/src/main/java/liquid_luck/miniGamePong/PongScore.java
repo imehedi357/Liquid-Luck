d3bb40aecfaf637cedd0b02ca2a5269719182a92
Ataur2019331077 <mdataurlikhon00@gmail.com>
2022-06-03 20:31:00 +0600

    pong

--- SUST_Life/src/main/java/liquid_luck/miniGamePong/PongScore.java
+++ SUST_Life/src/main/java/liquid_luck/miniGamePong/PongScore.java
@@ -1,11 +1,3 @@
-
-
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package liquid_luck.miniGamePong;
 import javax.swing.JFrame;
 import java.awt.*;
