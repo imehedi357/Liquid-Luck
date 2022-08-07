fdba59ce6ea591ca4664e35745057d037529faae
imehedi357 <imehedi357@gmail.com>
2022-08-07 08:02:42 +0600

    updates

--- SUST_Life/src/main/java/liquid_luck/sust_life/SUST_Life.java
+++ SUST_Life/src/main/java/liquid_luck/sust_life/SUST_Life.java
@@ -4,12 +4,7 @@ package liquid_luck.sust_life;
 
 import javax.swing.JFrame;
 
-import liquid_luck.miniGamePong.PongGameFrame;
 
-<<<<<<< HEAD
-=======
-
->>>>>>> b6ea2ee7324ba1796c9b3d6541580cd66e5317d0
 
 /**
  * SUST Life
@@ -36,13 +31,8 @@ public class SUST_Life {
 
         // Running the Gameloop
         gamePanel.startGameThread();
-<<<<<<< HEAD
-        //PongGameFrame pongFrame = new PongGameFrame();
-        new PongGameFrame();
-=======
-
-        //PongGameFrame pong = new PongGameFrame();
->>>>>>> b6ea2ee7324ba1796c9b3d6541580cd66e5317d0
-        
+        //PongGameFrame p = new PongGameFrame();
     }
+
+    
 }
